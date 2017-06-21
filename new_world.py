8f07c64bf6882153d6e94f63cae37e83f9bc7c5a
Szymen <szymmaslowski@gmail.com>
2017-06-21 15:48:40 +0200

    Plastyczne zmiany

--- new_world.py
+++ new_world.py
@@ -1,57 +1,15 @@
+import os
 
-
+from Frame import Frame
 import matplotlib.pyplot as plt
-
+import gold_master as gold
 from mpi4py import MPI
 
+desired_size = 512   # TODO: to ma byc sciagane z parametrow wywolania : )
+precision = 0.3
+file_name = "pi_digits/10e4.txt"
+folder_for_pics = "pics"
 
-def paint_gold(frame):
-    channels = 1
-    size = frame.__len__()
-
-
-    if size == 16:
-        pass
-
-    print ("GOLD FRAME!"+str(frame))
-
-
-def calc_diff_from_gold(frame):
-    opp_gold = 0.61833989
-    len = 0
-    ones = 0
-
-    for part in frame:
-        for x in part:
-            len += 1
-            if x == "1":
-                ones += 1
-    result = opp_gold - ones/len
-    return result
-
-
-def data_to_bin(data):
-    binary_vals = {
-        0: "0000",
-        1: "0001",
-        2: "0010",
-        3: "0011",
-        4: "0100",
-        5: "0101",
-        6: "0110",
-        7: "0111",
-        8: "1000",
-        9: "1001"}
-    return binary_vals[int(data)]
-
-
-def frame_push( frame, data):
-    for x in range(1,frame.__len__()):
-        frame[x-1] = frame[x]
-    frame[-1] = data
-
-
-file_name = "10000.txt"
 comm = MPI.COMM_WORLD
 world_size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
@@ -62,51 +20,55 @@ if rank == 0:
     pi_text = pi_text.read().replace(".", "")
     dx = int(pi_text.__len__()/world_size)
     data = list()
+    if dx < desired_size:
+        print("Desired frame size cant be bigger than calculating units.")
+        os.exit()
     for x in range(world_size):
-        data.append(pi_text[dx*x: dx*(x+1)])
+        begin = dx * x
+        end = dx * (x + 1)
+        if x != 0:
+            begin -= desired_size - 1
+        if x != world_size-1: # last one, numbered from 0
+            end += desired_size - 1
+        data.append(pi_text[begin:end])
 
 else:
     data = None
 
 data = comm.scatter(data, root = 0)
-result4 = []        # TODO: o roznych dlugosciach
-result16 = []
+result = []
 
-frame4 =[[],[],[],[]]
-frame16 = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+
+frame = Frame(desired_size)
+
 
 for data_pack in data:
-    frame_push(frame4, data_to_bin(data_pack))
-    frame_push(frame16, data_to_bin(data_pack))
-    res = calc_diff_from_gold(frame4)
-    result4.append(res)
-    if res < 0.1:  # TODO: mozna zadac prog dokladnosci
-        paint_gold(frame4)
-    res = calc_diff_from_gold(frame16)
-    result16.append(res)
-    if res < 0.1:
-        paint_gold(frame16)
+    frame.data_push(data_pack)
+    res = gold.calc_diff_from_gold(frame)
+    result.append(res)
+    if res < precision:
+        gold.paint_gold(frame, folder_for_pics)
 
-results_4 = comm.gather((result4, rank), root = 0)
-results_16 = comm.gather((result16,rank), root = 0)
+
+results = comm.gather((result, rank), root = 0)
+
 
 if rank == 0:
-    res_map_16 = {}
-    res_map_4 = {}
-    for x in results_4:
-        res_map_4[x[1]] = x[0]
-    for x in results_16:
-        res_map_16[x[1]] = x[0]
+    res_map = {}
+    for x in results:
+        res_map[x[1]] = x[0]
 
-    x = range(pi_text.__len__())
+    #x = range(pi_text.__len__() + desired_size*world_size - world_size*2 + 2) # TODO: nie wiem ile generuje danych i czy nie jest za duzo
     y = []
+
     for i in range(world_size):
-        for el in res_map_4[i]:
+        for el in res_map[i]:
             y.append(el)
 
-    plt.autoscale(enable = True)
+    x = range(y.__len__())
+    plt.autoscale(enable=True)
     plt.plot(x, y)
-    plt.savefig("Mean deviation_from_{}.png".format(file_name), dpi = 300)
+    plt.savefig("mean_deviation_graph.png".format(), dpi = 300)
     plt.close()
 
 
